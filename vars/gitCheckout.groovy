6694ee1cd88e6c9061546e73258dbd3149120d9f
Emman <emmanelm73@gmail.com>
2023-09-05 00:26:35 +0530

    checking

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map stageParams){
 
     checkout([
-            $class: 'GitSCMpoll',
+            $class: 'GitSCM',
             branches: [[name: stageParams.branch]],
             userRemoteConfigs: [[url: stageParams.url]]
     ])
